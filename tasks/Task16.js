2183ed34eedb922dcaa182721cad32c5b90bf55a
OmarAltoom <toom81138@gmail.com>
2025-02-12 14:22:34 +0300

    task16css

--- tasks/Task16.js
+++ tasks/Task16.js
@@ -1,14 +1,15 @@
 import React, { Component  } from 'react';
-import { View, Text ,Button,Alert} from 'react-native';
+import { View, Text ,Button,Alert, StyleSheet} from 'react-native';
 
 
 
-
+//in the class the code inide render will be excuted
 class Task16 extends Component {
   constructor(){
     super();
     this.state = {
-      content: true
+      content: true,
+      name: "omar"
     };
   }
   hide_show = () => {
@@ -18,7 +19,8 @@ class Task16 extends Component {
  render(){ 
   //{ this.state.content && <Text>Hello from Task16!</Text> || <Text></Text>}
   return (
-    <View>
+     
+    <View style={styles.container}>
       
       {this.state.content?<Text>Hello from Task16!</Text>:<Text></Text>}
       <Button
@@ -34,4 +36,21 @@ class Task16 extends Component {
   );
 };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f9fa',
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#2c3e50',
+    marginBottom: 20,
+  }
+});
+
 export default Task16;
